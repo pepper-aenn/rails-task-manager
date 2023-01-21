0376fc8012cd691d7b94a0af70b80a4559598d1b
anna schäffner <anna-schaeffner@gmx.de>
2023-01-21 16:49:16 +0100

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,16 @@ class TasksController < ApplicationController
     redirect_to tasks_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
+    redirect_to tasks_path(@task)
+  end
+
 private
 
   def tasks_params
